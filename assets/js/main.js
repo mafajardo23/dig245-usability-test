f5cc6cc8132b2fe3c6534cf01fff58f726e867cd
mafajardo23 <mafajardo@davidson.edu>
2023-10-24 16:31:57 -0400

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,11 +14,21 @@ myForm.addEventListener("submit", (event) => {
     let q8 = this.q8.value;
     let q9 = this.q9.value;
     let q10 = this.q10.value;
+
+    q1 -= 1;
+    q3 -= 1;
+    q5 -= 1;
+    q7 -= 1;
+    q9 -= 1;  
+    q2 = 5 - q2;
+    q4 = 5 - q4;
+    q6 = 5 - q6;
+    q8 = 5 - q8;
+    q10 = 5 - q10;
      
   let answer = (q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 + q10) * 2.5;
   document.querySelector('#answer').value = answer;
-  console.log(answer);
-  answerInput.value = answer;
+  
 
 });
 
